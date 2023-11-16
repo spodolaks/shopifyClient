0523514a743645c6cba82b39ed60d5aec8af041d
Sergejs Podolaks <sergejspodolaks@Sergejss-MacBook-Pro-2.local>
2023-11-16 10:27:57 +0200

    adding product structure

--- main.go
+++ main.go
@@ -1,13 +1,17 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
-	"network"
+	"net/http"
 	"os"
 
+	"github.com/spodolaks/shopifyClient/models"
+	"github.com/spodolaks/shopifyClient/network"
+
 	"github.com/joho/godotenv"
-	"github.com/spodolaks/shopifyGo/network"
 )
 
 func main() {
@@ -18,5 +22,27 @@ func main() {
   
 	token := os.Getenv("TOKEN")
 	network.Initialize();
-	fmt.Println(token)
+	client := network.Client
+	url := "https://spodolaks.myshopify.com/admin/api/2023-10/products.json"
+	req, _ := http.NewRequest(http.MethodGet, url, nil)
+	req.Header.Add("content-type", "application/json")
+	req.Header.Add("X-Shopify-Access-Token", token)
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+
+    // Read the response body
+    responseBody, err := ioutil.ReadAll(resp.Body)
+    if err != nil {
+        log.Fatal(err)
+    }
+	products := []models.Product
+	err := json.Unmarshal([]byte(jsonResponse), &products)
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    fmt.Println(string(responseBody))
 }
---
+++ models/product.go
@@ -0,0 +1,64 @@
+package models
+
+type Product struct {
+    ID                int64       `json:"id"`
+    Title             string      `json:"title"`
+    BodyHTML          string      `json:"body_html"`
+    Vendor            string      `json:"vendor"`
+    ProductType       string      `json:"product_type"`
+    CreatedAt         string      `json:"created_at"`
+    Handle            string      `json:"handle"`
+    UpdatedAt         string      `json:"updated_at"`
+    PublishedAt       string      `json:"published_at"`
+    TemplateSuffix    string      `json:"template_suffix"`
+    PublishedScope    string      `json:"published_scope"`
+    Tags              string      `json:"tags"`
+    Status            string      `json:"status"`
+    AdminGraphqlAPIID string      `json:"admin_graphql_api_id"`
+    Variants          []Variant   `json:"variants"`
+    Options           []Option    `json:"options"`
+    Images            []Image     `json:"images"`
+    Image             string		 `json:"image"`
+}
+
+type Variant struct {
+    ID                   int64       `json:"id"`
+    ProductID            int64       `json:"product_id"`
+    Title                string      `json:"title"`
+    Price                string      `json:"price"`
+    SKU                  string      `json:"sku"`
+    Position             int         `json:"position"`
+    InventoryPolicy      string      `json:"inventory_policy"`
+    CompareAtPrice       string      `json:"compare_at_price"`
+    FulfillmentService   string      `json:"fulfillment_service"`
+    InventoryManagement  string      `json:"inventory_management"`
+    Option1              string      `json:"option1"`
+    Option2              string      `json:"option2"`
+    Option3              string      `json:"option3"`
+    CreatedAt            string      `json:"created_at"`
+    UpdatedAt            string      `json:"updated_at"`
+    Taxable              bool        `json:"taxable"`
+    Barcode              string      `json:"barcode"`
+    Grams                int         `json:"grams"`
+    ImageID              string		 `json:"image_id"`
+    Weight               float64     `json:"weight"`
+    WeightUnit           string      `json:"weight_unit"`
+    InventoryItemID      int64       `json:"inventory_item_id"`
+    InventoryQuantity    int         `json:"inventory_quantity"`
+    OldInventoryQuantity int         `json:"old_inventory_quantity"`
+    RequiresShipping     bool        `json:"requires_shipping"`
+    AdminGraphqlAPIID    string      `json:"admin_graphql_api_id"`
+}
+
+type Option struct {
+    ID        int64    `json:"id"`
+    ProductID int64    `json:"product_id"`
+    Name      string   `json:"name"`
+    Position  int      `json:"position"`
+    Values    []string `json:"values"`
+}
+
+type Image struct {
+    Url string `json:"url"`
+}
+
